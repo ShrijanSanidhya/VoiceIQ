670adcc3445b06e971e2c0c1dd7203efe3b7b95e
“Shrijan <shrisan0710@gmail.com>
2026-04-22 09:40:56 +0530

    feat: Add setup file frontend/src/pages/Home.jsx

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Home Page Component
+// This will serve as the landing page for VoiceIQ
+const Home = () => {
+  return (
+    <div className="p-4">
+      {/* TODO: Implement Home page UI */}
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+
+export default Home;
