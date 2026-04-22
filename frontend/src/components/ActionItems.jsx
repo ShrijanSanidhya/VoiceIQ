5edd6a477058e6816ddbe82a0af39f5e51639e58
“Shrijan <shrisan0710@gmail.com>
2026-04-22 09:40:56 +0530

    feat: Add setup file frontend/src/components/ActionItems.jsx

---
+++ frontend/src/components/ActionItems.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// ActionItems Component
+// Extracts and displays actionable items from the summary
+const ActionItems = () => {
+  return (
+    <div>
+      {/* TODO: List out action items with checkboxes */}
+      <div>Action Items placeholder</div>
+    </div>
+  );
+};
+
+export default ActionItems;
