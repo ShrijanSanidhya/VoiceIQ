a158df33f908a166b363549430e78d6bb1e50e77
“Shrijan <shrisan0710@gmail.com>
2026-04-22 09:40:56 +0530

    feat: Add setup file frontend/src/components/Waveform.jsx

---
+++ frontend/src/components/Waveform.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Waveform Component
+// Uses wavesurfer.js to display audio visualization
+const Waveform = () => {
+  return (
+    <div>
+      {/* TODO: Initialize WaveSurfer instance here */}
+      <div>Audio Waveform placeholder</div>
+    </div>
+  );
+};
+
+export default Waveform;
